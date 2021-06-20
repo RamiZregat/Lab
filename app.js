3b06577ad14c33cd1dff353ce5a33141fce2d3de
RamiZregat <rami.zregat97@gmail.com>
2021-06-20 16:30:42 +0300

    first  push

---
+++ app.js
@@ -0,0 +1,9 @@
+let UserFood=prompt('What is your favorite food?');
+alert('You choose '+ UserFood + ' as your favorite food');
+let UserDrink=prompt('What kind of drinks do you like?');
+alert('You choose '+ UserDrink + ' as your favorable drink');
+let UserSalad=prompt('what kind of salad you like to have with your meals?');
+alert('You choose '+ UserSalad);
+let UserPlace=prompt('Where do you prefer eating your meals?');
+alert('You prefer '+ UserPlace + ' as your favorable place to eat your meal');
+alert('You have won with us a free '+ UserFood + ' meal with ' + UserDrink + ' drink with ' + UserSalad + ' in ' + UserPlace + ' Hope you a great time and enjoy your meal' );
